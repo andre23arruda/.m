99d2c8b2096ece57ec2a4117c99d581af42dc09f
andre23arruda <33252657+andre23arruda@users.noreply.github.com>
2018-11-28 10:56:13 -0200

    Create fechatudo.m

---
+++ fechatudo.m
@@ -0,0 +1,9 @@
+function fechatudo()
+
+% Funcao criada para fechar todas as janelas existentes no universo do
+% matlab. coisa chata da porra. tnc
+    k = waitforbuttonpress;
+    if (k ~= 0)
+        close all;
+    end
+end
